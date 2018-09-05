180bf4c8c8a70ef0547bb6163344fce5da26477c
zmil44 <zachary.j.miller@outlook.com>
2018-09-05 09:37:12 -0400

    Reads from a file Step 2 of #5

--- MarkovText/MarkovModel.cs
+++ MarkovText/MarkovModel.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace MarkovText
 {
+    struct FirstCharacter
+    {
+
+    }
+
+    struct MiddleCharacter
+    {
+
+    }
+
+    struct EndCharacter
+    {
+
+    }
+
     class MarkovModel
     {
+        private FirstCharacter[] firstCharacters = new FirstCharacter[26];
+        private MiddleCharacter[] middleCharacters = new MiddleCharacter[26];
+        private EndCharacter[] endCharacters = new EndCharacter[26];
+
+        public void AddWord(string word)
+        {
+
+            if (word.Length > 3)
+            {
+                AddFirstCharacter();
+                AddMiddleCharacters();
+                AddEndCharacter();
+            }
+        }
+
+        private void AddFirstCharacter()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddMiddleCharacters()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddEndCharacter()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- MarkovText/Program.cs
+++ MarkovText/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,18 @@ namespace MarkovText
     {
         static void Main(string[] args)
         {
+            //the path three directories above teh executable directory in VS
+            //from https://stackoverflow.com/a/14549805
+            string wanted_path = Path.GetDirectoryName(
+                    Path.GetDirectoryName(
+                        Path.GetDirectoryName(
+                            Directory.GetCurrentDirectory())));
+            string filePath = $"{wanted_path}\\words_alpha.txt";
+            var lines = File.ReadAllLines(filePath);
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine(lines[i]);
+            }
         }
     }
 }
